6410127a4630da266b6d7ff87b3ef0866531c677
Retrontology <retrontology@hotmail.com>
2022-02-21 02:15:04 -0600

    fix saving and empty object creation

--- reyaml.py
+++ reyaml.py
@@ -3,14 +3,16 @@ import yaml
 
 class yamlConf(dictObj):
 
-    def __init__(self, path):
+    def __init__(self, path = None):
         self.path = path
         self.load()
 
     def load(self):
-        with open(self.path, 'r') as stream:
-            super().__load__(yaml.safe_load(stream))
+        if self.path:
+            with open(self.path, 'r') as stream:
+                super().__load__(yaml.safe_load(stream))
 
     def save(self):
+        out = self.__to_dict__().copy()
         with open(self.path, 'w') as stream:
-            stream.write(yaml.safe_dump(self.__to_dict__().copy()))
+            stream.write(yaml.safe_dump(out))
